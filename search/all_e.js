4ada4ca91ab7c038004c0867775cbfa6ab6b9b88
Tasssadar <Tasssadar@users.noreply.github.com>
2020-07-08 13:48:04 +0000

    deploy: bc5bba01f4924622a8f7ce031e6fca53e03d250b

--- search/all_e.js
+++ search/all_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sledování_20čáry_98',['Sledování čáry',['../group__line.html',1,'']]],
+  ['sledování_20čáry_20pomocí_20senzorické_20lišty_2e_98',['Sledování čáry pomocí senzorické lišty.',['../group__line.html',1,'']]],
   ['set_99',['set',['../classinternal_1_1buffer.html#a0c5d6939cc27e8be6ae5e7bede1e3ae8',1,'internal::buffer']]],
   ['size_100',['size',['../classbasic__string__view.html#a7fd9f4dcac1782259b0cd217af69d737',1,'basic_string_view::size()'],['../classinternal_1_1buffer.html#abac844b364cd93450a9c311807d15f80',1,'internal::buffer::size()'],['../classformat__int.html#a644fe6f2d0505d48ff08ab22448b5069',1,'format_int::size()']]],
   ['str_101',['str',['../classformat__int.html#ab14204962423d35a320f15e673b08e06',1,'format_int']]],
--- search/groups_5.js
+++ search/groups_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['sledování_20čáry_222',['Sledování čáry',['../group__line.html',1,'']]]
+  ['sledování_20čáry_20pomocí_20senzorické_20lišty_2e_222',['Sledování čáry pomocí senzorické lišty.',['../group__line.html',1,'']]]
 ];
