34e2d2c6b35d69e080bf11c95cd667a7fede3be1
Tasssadar <Tasssadar@users.noreply.github.com>
2023-05-30 18:30:52 +0000

    deploy: f88fd78bc2ab76353506f6c3f5a64607fce8f704

--- search/all_f.js
+++ search/all_f.js
@@ -1,13 +1,14 @@
 var searchData=
 [
-  ['serva_20_28hloupá_29_0',['Serva (hloupá)',['../group__stupidservos.html',1,'']]],
-  ['set_1',['set',['../classinternal_1_1buffer.html#a0c5d6939cc27e8be6ae5e7bede1e3ae8',1,'internal::buffer']]],
-  ['size_2',['size',['../classbasic__string__view.html#a7fd9f4dcac1782259b0cd217af69d737',1,'basic_string_view::size()'],['../classinternal_1_1buffer.html#abac844b364cd93450a9c311807d15f80',1,'internal::buffer::size()'],['../classformat__int.html#a644fe6f2d0505d48ff08ab22448b5069',1,'format_int::size()']]],
-  ['sledování_20čáry_20_28_21_21_20ir_20moduly_20_21_21_29_3',['Sledování čáry (!! IR moduly !!)',['../group__irmodules.html',1,'']]],
-  ['sledování_20čáry_20_28_21_21_20senzorická_20lišta_20_21_21_29_4',['Sledování čáry (!! senzorická lišta !!)',['../group__line.html',1,'']]],
-  ['smart_5fleds_5fcount_5',['smart_leds_count',['../structrkConfig.html#a3090e05f82be0896899460ea37855c43',1,'rkConfig']]],
-  ['str_6',['str',['../classformat__int.html#ab14204962423d35a320f15e673b08e06',1,'format_int']]],
-  ['stupid_5fservo_5fmax_7',['stupid_servo_max',['../structrkConfig.html#a05e04d297ef74b69ea1d1b30d2f11eec',1,'rkConfig']]],
-  ['stupid_5fservo_5fmin_8',['stupid_servo_min',['../structrkConfig.html#a9981cee3b3e22a70bdc8b0ff8f052ded',1,'rkConfig']]],
-  ['system_5ferror_9',['system_error',['../classsystem__error.html',1,'system_error'],['../classsystem__error.html#a6761fd4c8604a59a7c79f6bfdfaf25cb',1,'system_error::system_error()']]]
+  ['serva_20_28chytrá_29_0',['Serva (chytrá)',['../group__smartservos.html',1,'']]],
+  ['serva_20_28hloupá_29_1',['Serva (hloupá)',['../group__stupidservos.html',1,'']]],
+  ['set_2',['set',['../classinternal_1_1buffer.html#a0c5d6939cc27e8be6ae5e7bede1e3ae8',1,'internal::buffer']]],
+  ['size_3',['size',['../classbasic__string__view.html#a7fd9f4dcac1782259b0cd217af69d737',1,'basic_string_view::size()'],['../classinternal_1_1buffer.html#abac844b364cd93450a9c311807d15f80',1,'internal::buffer::size()'],['../classformat__int.html#a644fe6f2d0505d48ff08ab22448b5069',1,'format_int::size()']]],
+  ['sledování_20čáry_20_28_21_21_20ir_20moduly_20_21_21_29_4',['Sledování čáry (!! IR moduly !!)',['../group__irmodules.html',1,'']]],
+  ['sledování_20čáry_20_28_21_21_20senzorická_20lišta_20_21_21_29_5',['Sledování čáry (!! senzorická lišta !!)',['../group__line.html',1,'']]],
+  ['smart_5fleds_5fcount_6',['smart_leds_count',['../structrkConfig.html#a3090e05f82be0896899460ea37855c43',1,'rkConfig']]],
+  ['str_7',['str',['../classformat__int.html#ab14204962423d35a320f15e673b08e06',1,'format_int']]],
+  ['stupid_5fservo_5fmax_8',['stupid_servo_max',['../structrkConfig.html#a05e04d297ef74b69ea1d1b30d2f11eec',1,'rkConfig']]],
+  ['stupid_5fservo_5fmin_9',['stupid_servo_min',['../structrkConfig.html#a9981cee3b3e22a70bdc8b0ff8f052ded',1,'rkConfig']]],
+  ['system_5ferror_10',['system_error',['../classsystem__error.html',1,'system_error'],['../classsystem__error.html#a6761fd4c8604a59a7c79f6bfdfaf25cb',1,'system_error::system_error()']]]
 ];
--- search/groups_6.js
+++ search/groups_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['serva_20_28hloupá_29_0',['Serva (hloupá)',['../group__stupidservos.html',1,'']]],
-  ['sledování_20čáry_20_28_21_21_20ir_20moduly_20_21_21_29_1',['Sledování čáry (!! IR moduly !!)',['../group__irmodules.html',1,'']]],
-  ['sledování_20čáry_20_28_21_21_20senzorická_20lišta_20_21_21_29_2',['Sledování čáry (!! senzorická lišta !!)',['../group__line.html',1,'']]]
+  ['serva_20_28chytrá_29_0',['Serva (chytrá)',['../group__smartservos.html',1,'']]],
+  ['serva_20_28hloupá_29_1',['Serva (hloupá)',['../group__stupidservos.html',1,'']]],
+  ['sledování_20čáry_20_28_21_21_20ir_20moduly_20_21_21_29_2',['Sledování čáry (!! IR moduly !!)',['../group__irmodules.html',1,'']]],
+  ['sledování_20čáry_20_28_21_21_20senzorická_20lišta_20_21_21_29_3',['Sledování čáry (!! senzorická lišta !!)',['../group__line.html',1,'']]]
 ];
