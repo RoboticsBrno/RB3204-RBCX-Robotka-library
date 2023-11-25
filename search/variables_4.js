1fc33b8d31c08683e4b662503c4154a161cfcb4f
Tasssadar <Tasssadar@users.noreply.github.com>
2023-11-25 20:55:09 +0000

    deploy: ba29543726dca8b8e4ee69cdec8ad9afd3416fd6

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['rbcontroller_5fapp_5fenable_0',['rbcontroller_app_enable',['../structrkConfig.html#a18347342033debb9e6b6a0d2323ed96a',1,'rkConfig']]]
+  ['rbcontroller_5fapp_5fenable_0',['rbcontroller_app_enable',['../structrkConfig.html#a18347342033debb9e6b6a0d2323ed96a',1,'rkConfig']]],
+  ['rbcontroller_5fenable_5fudp_1',['rbcontroller_enable_udp',['../structrkConfig.html#aa87b5e23cb356c8e3c9080d4de976038',1,'rkConfig']]],
+  ['rbcontroller_5fenable_5fwebocket_2',['rbcontroller_enable_webocket',['../structrkConfig.html#a7ae9a4cea97ad0709da187de48208a19',1,'rkConfig']]]
 ];
